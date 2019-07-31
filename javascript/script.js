83e3b6bd222a50d11807ec3c868b44668b1180f8
Krystian Konecki <krystian_konecki@wp.pl>
2019-07-31 17:41:21 +0200

    Adding global section

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,16 @@
 const barIcon = document.querySelector('.main-side__bar-icon');
 const navClass = document.querySelector('.menu');
+const globalCircleList = document.querySelectorAll('[data-id]');
 
 //Bar
-barIcon.addEventListener('click',() => {
+barIcon.addEventListener('click', () => {
     navClass.classList.toggle('menu--active')
 });
+
+//Global 
+
+Array.from(globalCircleList).forEach((element) => {
+    element.addEventListener('click', () => {
+            element.classList.toggle('global__circle--active')
+        })
+})
